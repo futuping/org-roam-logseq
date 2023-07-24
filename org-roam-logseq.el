57ca0ecf2c83982cf87426569607eddec7e0780d
futuping <45912467+futuping@users.noreply.github.com>
2023-07-25 02:48:05 +0800

    Update org-roam-logseq.el

--- org-roam-logseq.el
+++ org-roam-logseq.el
@@ -21,9 +21,6 @@
 ;; it won't convert the resulting links.
 ;;
 
-;; (require 'f)
-(use-package f)
-;; (require 'org-roam)
 
 ;; Your logseq directory should be inside your org-roam directory,
 ;; put the directory you use here
